a6d3c65fc5491fae2b128590ad24c910484d6a92
FriendlyUser1 <ciirodev335@gmail.com>
2022-12-04 23:39:10 +0000

    2015 day 8

---
+++ 2015/day8/strlen.js
@@ -0,0 +1,20 @@
+const fs = require("fs"),
+	input = fs.readFileSync("./input.txt", "utf-8").split("\n");
+
+let codelen = 0,
+	strlen = 0;
+
+for (let i = 0; i < input.length; i++) {
+	codelen += input[i].length;
+	strlen += eval(input[i]).length;
+}
+
+console.log(codelen - strlen);
+
+let encodelen = 0;
+
+for (let i = 0; i < input.length; i++) {
+	encodelen += JSON.stringify(input[i]).length;
+}
+
+console.log(encodelen - codelen);
