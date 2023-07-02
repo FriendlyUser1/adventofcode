a4cb0ae93d24a052c5acd4a5494b4a5ba5efe173
FriendlyUser1 <ciirodev335@gmail.com>
2023-07-02 10:20:41 +0100

    2017 day 2

---
+++ 2017/day2/checksum.js
@@ -0,0 +1,26 @@
+const rows = require("fs")
+	.readFileSync("./input.txt", "utf-8")
+	.split("\n")
+	.map((r) => r.match(/[0-9]+/g).map((n) => parseInt(n)));
+
+let checksum = 0,
+	divisible = 0;
+
+for (let i = 0; i < rows.length; i++) {
+	let rowmax = 0,
+		rowmin = rows[i][0];
+
+	for (let j = 0; j < rows[i].length; j++) {
+		let n = rows[i][j];
+		if (n > rowmax) rowmax = n;
+		if (n < rowmin) rowmin = n;
+
+		for (let k = 0; k < rows[i].length; k++)
+			if (n != rows[i][k] && n % rows[i][k] === 0) divisible += n / rows[i][k];
+	}
+
+	checksum += Math.abs(rowmax - rowmin);
+}
+
+console.log(`The checksum is ${checksum}`);
+console.log(`The sum of divisible numbers is ${divisible}`);
