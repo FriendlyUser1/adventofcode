552e3517cce7fead0c8a9f5ee9d01057b21da51f
ciiro <ciirodev335@gmail.com>
2024-11-07 00:38:26 +0000

    2018 days 1 and 2

---
+++ 2018/day1/frequency.js
@@ -0,0 +1,21 @@
+const input = require("fs").readFileSync("./input.txt", "utf-8").split("\n");
+
+// Part 1
+const sum = input.reduce((prev, cur) => prev + parseInt(cur), 0);
+
+// Part 2
+// Note: don't use an array! (Switching to a set: 6 seconds -> 15ms)
+let freq = 0;
+let repeat = null;
+const freqs = new Set([0]);
+while (repeat == null) {
+	input.reduce((prev, cur) => {
+		freq = prev + parseInt(cur);
+		if (freqs.has(freq) && repeat == null) repeat = freq;
+		else freqs.add(freq);
+		return freq;
+	}, freq);
+}
+
+console.log("Sum: ", sum);
+console.log("First repeated frequency:", repeat);
---
+++ 2018/day2/boxes.js
@@ -0,0 +1,51 @@
+let input = require("fs").readFileSync("./input.txt", "utf-8").split("\n");
+
+// Part 1
+let count2 = 0;
+let count3 = 0;
+
+for (let i = 0; i < input.length; i++) {
+	let id = input[i].split("").sort(),
+		has2 = false,
+		has3 = false,
+		last = id[0],
+		lcount = 1;
+
+	for (let j = 1; j < id.length; j++) {
+		let current = id[j];
+
+		if (current == last) lcount++;
+
+		if ((current != last && lcount > 0) || j == id.length - 1) {
+			if (lcount == 2) has2 = true;
+			if (lcount == 3) has3 = true;
+			lcount = 1;
+		}
+
+		last = current;
+	}
+
+	if (has2) count2++;
+	if (has3) count3++;
+}
+
+console.log("Checksum: ", count2 * count3);
+
+// Part 2
+input = input.map((id) => id.split(""));
+
+for (let i = 1; i < input.length - 1; i++) {
+	let ids = input.slice(i);
+	let checkid = input[i - 1];
+	for (let j = 0; j < checkid.length; j++) {
+		let commonid = checkid.toSpliced(j, 1).join("");
+		let matches = ids.filter(
+			(fullid) => fullid.toSpliced(j, 1).join("") == commonid
+		);
+
+		if (matches.length > 0) {
+			console.log("Found match. Common letters:", commonid);
+			return;
+		}
+	}
+}
