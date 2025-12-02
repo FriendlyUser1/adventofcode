cd0fb598eb7bc77dee535ad22eec0d960a1e8860
FriendlyUser1 <ciirodev335@gmail.com>
2025-12-02 23:23:30 +0000

    2025 day 1

---
+++ 2025/day1/dial.js
@@ -0,0 +1,21 @@
+import { readFileSync } from "node:fs";
+const input = readFileSync("./input.txt", "utf-8").split("\n");
+
+let clickZeroes = 0;
+let endZeroes = 0;
+let last = 50;
+
+for (let i = 0; i < input.length; i++) {
+	let rotate = parseInt(input[i].substring(1));
+	let direction = input[i].includes("L") ? -1 : 1;
+
+	for (; rotate > 0; rotate--) {
+		last += 1 * direction;
+		last = ((last % 100) + 100) % 100;
+		if (last == 0) clickZeroes++;
+	}
+	if (last == 0) endZeroes++;
+}
+
+console.log("Password (part 1) is", endZeroes);
+console.log("Password (part 2) is", clickZeroes);
